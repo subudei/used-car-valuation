b61dc9d36712508cf3c5ee71162227b28b49e4d5
subudei <milan_cirkovic@yahoo.com>
2024-07-08 17:19:34 +0200

    feat(users): Add endpoint for user signup,  create-user.dto, request.http to check requests

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // injectRepository User is used for generic type Repository<User>
+  // repo is argument name, Repository<User> is the type
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // create is a method from the repo object
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}

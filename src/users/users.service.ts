2892adf9f960556a10d8e68e9789f6d8dbe9bda6
subudei <milan_cirkovic@yahoo.com>
2024-07-09 15:19:15 +0200

    chore: Inject User repository into UsersService for database operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,13 +5,21 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  // injectRepository User is used for generic type Repository<User>
-  // repo is argument name, Repository<User> is the type
+  /*
+  InjectRepository is a decorator provided by the TypeORM library. It is used to inject a repository instance into the UsersService class.
+  Repositories in TypeORM are responsible for handling database operations for a specific entity. They provide methods for querying, creating, updating, and deleting records in the corresponding database table.
+  By using the InjectRepository decorator, we are instructing the dependency injection system to provide an instance of the Repository<User> class when creating an instance of the UsersService class. The Repository<User> type specifies that the repository is specifically for the User entity.
+  The @InjectRepository(User) decorator is applied to the repo parameter in the constructor of the UsersService class. This tells the dependency injection system to inject an instance of the Repository<User> class into the repo parameter.
+  Once the repository is injected, it can be used within the UsersService class to perform database operations related to the User entity. In this case, the create method uses the injected repository to create a new user object and save it to the database.
+
+  Overall, the InjectRepository decorator simplifies the process of injecting a repository instance into a service class, allowing us to easily perform database operations within the service.
+*/
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
-    // create is a method from the repo object
+    // create() is used to create an entity instance with the specified data. It does not save the entity to the database immediately; it simply creates an instance of the entity with the provided data.
     const user = this.repo.create({ email, password });
+    // save() is used to persist the entity to the database. It takes the entity instance created using the create() method and saves it to the database table associated with the entity.
     return this.repo.save(user);
   }
 }
